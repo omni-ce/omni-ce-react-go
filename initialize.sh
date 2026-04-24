25bdeb25cf2c395217f8141538bd565f23c33a12
jefripunza <jefripunza@gmail.com>
2026-04-24 10:50:58 +0700

    feat: add progress logging and automated backend build step to initialization script

--- initialize.sh
+++ initialize.sh
@@ -15,16 +15,17 @@ done
 read -p "HTML Title? [title | mini-detail] : " htmlTitle
 read -p "HTML Description?                 : " htmlDescription
 
+echo "Start Process ..."
+
 # Remove git repository
 # rm -rf .git
 
 # Install dependencies
+echo "Install dependencies ..."
 bun i
 
-# Build frontend
-bun run build
-
 # Replace variables in application.variable.go
+echo "Replace variables ..."
 if [ ! -z "$htmlTitle" ]; then
   escTitle=$(printf '%s' "$htmlTitle" | sed -e 's/[\/&]/\\&/g')
   sed -i '' "s/AppHtmlHeadTitle       = \".*\"/AppHtmlHeadTitle       = \"$escTitle\"/" variable/application.variable.go
@@ -40,6 +41,18 @@ if [ ! -z "$projectCode" ] && [ "$projectCode" != "react-go" ]; then
   find . -type d \( -name .git -o -name node_modules \) -prune -false -o -type f -not -name 'initialize.sh' -not -name 'apocalypse.sh' | while read -r file; do
     if grep -q 'react-go' "$file" 2>/dev/null; then
       sed -i '' "s/react-go/$projectCode/g" "$file"
+      echo "Replace file: $file"
     fi
   done
 fi
+
+# Build frontend
+echo "Build frontend ..."
+bun run build
+
+# Build backend
+echo "Build backend ..."
+go build -o $projectCode main.go
+
+echo "Done ..."
+rm -rf $projectCode
