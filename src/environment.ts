522466a30b9d2edd5fdbe2a147eb5ef325d8eb30
jefripunza <jefripunza@gmail.com>
2026-04-25 13:23:44 +0700

    feat: add localhost support to environment API configuration

--- src/environment.ts
+++ src/environment.ts
@@ -3,3 +3,6 @@ export let HOST_API = env.VITE_HOST_API || "";
 if (window.location.hostname.includes("devtunnels.ms")) {
   HOST_API = "https://t7wxxqtb-1234.asse.devtunnels.ms";
 }
+if (window.location.hostname.includes("localhost")) {
+  HOST_API = "http://localhost:3000";
+}
