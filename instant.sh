737a3a3ac1a1e00b8d00d10b96c521f92aa6d834
jefripunza <jefripunza@gmail.com>
2026-04-25 13:18:50 +0700

    feat: add default admin credentials to initialization script

--- instant.sh
+++ instant.sh
@@ -4,6 +4,8 @@
 
 bash initialize.sh <<EOF
 navada-grosir
+admin
+admin@ABC123
 Navada Grosir | Sistem Manajemen Gudang Terpadu
 Aplikasi manajemen gudang profesional (WMS) untuk mencatat, mengecek stok barang, mengontrol inventaris, dan memantau operasional Navada Grosir secara real-time.
 EOF
