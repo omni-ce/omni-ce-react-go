9275c3fb4392df7a52be8a025e1791554a02b713
jefripunza <jefripunza@gmail.com>
2026-05-11 00:19:59 +0700

    refactor: update vite.config.ts to use double quotes and reorder react-icons chunk logic

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
-const limit_size_chunk = 10 // MB
+const limit_size_chunk = 10; // MB
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,41 +13,41 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           // chunk per file react-icons
-          if (id.includes('node_modules/react-icons/ai')) return 'icons-ai'
-          if (id.includes('node_modules/react-icons/bi')) return 'icons-bi'
-          if (id.includes('node_modules/react-icons/bs')) return 'icons-bs'
-          if (id.includes('node_modules/react-icons/cg')) return 'icons-cg'
-          if (id.includes('node_modules/react-icons/ci')) return 'icons-ci'
-          if (id.includes('node_modules/react-icons/di')) return 'icons-di'
-          if (id.includes('node_modules/react-icons/fa6')) return 'icons-fa6'
-          if (id.includes('node_modules/react-icons/fa')) return 'icons-fa'
-          if (id.includes('node_modules/react-icons/fc')) return 'icons-fc'
-          if (id.includes('node_modules/react-icons/fi')) return 'icons-fi'
-          if (id.includes('node_modules/react-icons/gi')) return 'icons-gi'
-          if (id.includes('node_modules/react-icons/go')) return 'icons-go'
-          if (id.includes('node_modules/react-icons/gr')) return 'icons-gr'
-          if (id.includes('node_modules/react-icons/hi2')) return 'icons-hi2'
-          if (id.includes('node_modules/react-icons/hi')) return 'icons-hi'
-          if (id.includes('node_modules/react-icons/im')) return 'icons-im'
-          if (id.includes('node_modules/react-icons/io5')) return 'icons-io5'
-          if (id.includes('node_modules/react-icons/io')) return 'icons-io'
-          if (id.includes('node_modules/react-icons/lia')) return 'icons-lia'
-          if (id.includes('node_modules/react-icons/lib')) return 'icons-lib'
-          if (id.includes('node_modules/react-icons/lu')) return 'icons-lu'
-          if (id.includes('node_modules/react-icons/md')) return 'icons-md'
-          if (id.includes('node_modules/react-icons/pi')) return 'icons-pi'
-          if (id.includes('node_modules/react-icons/ri')) return 'icons-ri'
-          if (id.includes('node_modules/react-icons/rx')) return 'icons-rx'
-          if (id.includes('node_modules/react-icons/si')) return 'icons-si'
-          if (id.includes('node_modules/react-icons/sl')) return 'icons-sl'
-          if (id.includes('node_modules/react-icons/tb')) return 'icons-tb'
-          if (id.includes('node_modules/react-icons/tfi')) return 'icons-tfi'
-          if (id.includes('node_modules/react-icons/ti')) return 'icons-ti'
-          if (id.includes('node_modules/react-icons/vsc')) return 'icons-vsc'
-          if (id.includes('node_modules/react-icons/wi')) return 'icons-wi'
+          if (id.includes("node_modules/react-icons/ai")) return "icons-ai";
+          if (id.includes("node_modules/react-icons/bs")) return "icons-bs";
+          if (id.includes("node_modules/react-icons/bi")) return "icons-bi";
+          if (id.includes("node_modules/react-icons/ci")) return "icons-ci";
+          if (id.includes("node_modules/react-icons/cg")) return "icons-cg";
+          if (id.includes("node_modules/react-icons/di")) return "icons-di";
+          if (id.includes("node_modules/react-icons/fi")) return "icons-fi";
+          if (id.includes("node_modules/react-icons/fc")) return "icons-fc";
+          if (id.includes("node_modules/react-icons/fa")) return "icons-fa";
+          if (id.includes("node_modules/react-icons/fa6")) return "icons-fa6";
+          if (id.includes("node_modules/react-icons/gi")) return "icons-gi";
+          if (id.includes("node_modules/react-icons/go")) return "icons-go";
+          if (id.includes("node_modules/react-icons/gr")) return "icons-gr";
+          if (id.includes("node_modules/react-icons/hi")) return "icons-hi";
+          if (id.includes("node_modules/react-icons/hi2")) return "icons-hi2";
+          if (id.includes("node_modules/react-icons/im")) return "icons-im";
+          if (id.includes("node_modules/react-icons/lia")) return "icons-lia";
+          if (id.includes("node_modules/react-icons/io")) return "icons-io";
+          if (id.includes("node_modules/react-icons/io5")) return "icons-io5";
+          if (id.includes("node_modules/react-icons/lu")) return "icons-lu";
+          if (id.includes("node_modules/react-icons/md")) return "icons-md";
+          if (id.includes("node_modules/react-icons/pi")) return "icons-pi";
+          if (id.includes("node_modules/react-icons/rx")) return "icons-rx";
+          if (id.includes("node_modules/react-icons/ri")) return "icons-ri";
+          if (id.includes("node_modules/react-icons/si")) return "icons-si";
+          if (id.includes("node_modules/react-icons/sl")) return "icons-sl";
+          if (id.includes("node_modules/react-icons/tb")) return "icons-tb";
+          if (id.includes("node_modules/react-icons/tfi")) return "icons-tfi";
+          if (id.includes("node_modules/react-icons/ti")) return "icons-ti";
+          if (id.includes("node_modules/react-icons/vsc")) return "icons-vsc";
+          if (id.includes("node_modules/react-icons/wi")) return "icons-wi";
 
-          if (id.includes('node_modules')) return 'vendor'
-          if (id.includes('/src/pages/')) return 'pages'
+          if (id.includes("node_modules/react-icons/lib")) return "icons-lib";
+          if (id.includes("node_modules")) return "vendor";
+          if (id.includes("/src/pages/")) return "pages";
         },
       },
     },
@@ -55,18 +55,18 @@ export default defineConfig({
   // import alias
   resolve: {
     alias: {
-      '@': '/src',
+      "@": "/src",
     },
   },
   server: {
     watch: {
       ignored: [
-        '**/*.go',
-        '**/go.mod',
-        '**/go.sum',
-        '**/vendor/**',
-        '**/bin/**',
-        '**/tmp/**',
+        "**/*.go",
+        "**/go.mod",
+        "**/go.sum",
+        "**/vendor/**",
+        "**/bin/**",
+        "**/tmp/**",
       ],
     },
   },
@@ -74,10 +74,10 @@ export default defineConfig({
     react(),
     tailwindcss(),
     VitePWA({
-      strategies: 'injectManifest',
-      srcDir: 'src',
-      filename: 'sw.ts',
-      registerType: 'prompt',
+      strategies: "injectManifest",
+      srcDir: "src",
+      filename: "sw.ts",
+      registerType: "prompt",
       injectRegister: false,
 
       pwaAssets: {
@@ -86,23 +86,23 @@ export default defineConfig({
       },
 
       manifest: {
-        name: 'flash-builder-pwa-name',
-        short_name: 'flash-builder-pwa-short-name',
-        description: 'flash-builder-pwa-description',
-        theme_color: '#ffffff',
+        name: "flash-builder-pwa-name",
+        short_name: "flash-builder-pwa-short-name",
+        description: "flash-builder-pwa-description",
+        theme_color: "#ffffff",
       },
 
       injectManifest: {
-        globPatterns: ['**/*.{js,css,html,svg,png,ico}'],
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
         maximumFileSizeToCacheInBytes: 1024 * 1024 * limit_size_chunk,
       },
 
       devOptions: {
         enabled: false,
-        navigateFallback: 'index.html',
+        navigateFallback: "index.html",
         suppressWarnings: true,
-        type: 'module',
+        type: "module",
       },
     }),
   ],
-})
+});
